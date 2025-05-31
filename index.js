2f9c8dd6c3af7c12975e6d995f99b041abc6b7cc
EduardoM <eduardomeza540@gmail.com>
2025-05-30 21:48:49 -0700

    added needed functionality

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const addBook = document.querySelector('.addBook');
 
 const myLibrary = [];
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function(){
-    const info = `${this.title} by ${this.author} has ${this.pages} pages. ${read? 'Already read.': "Haven't read yet."}`;
-    return info;
-  }
   this.id = crypto.randomUUID();
 }
 
@@ -20,25 +20,59 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(newBook);
 }
 
-addBookToLibrary('hello', 'me', 12, true);
-addBookToLibrary('there', 'you', 5, false);
+function removeBook(id) {
+  const index = myLibrary.findIndex(book => book.id === id);
+  if (index !== -1) {
+    myLibrary.splice(index, 1);
+    displayBooks();
+  }
+}
 
-function displayBook() {
+function displayBooks() {
     display.innerHTML = "";
 
     myLibrary.forEach(book => {
         const div = document.createElement('div');
+        div.classList.add('book-card');
+        div.setAttribute('data-id', book.id);
         div.innerHTML = `
+        <span class="delete">🗙</span>
         <ul>
             <li>Title: ${book.title}</li>
             <li>Author: ${book.author}</li>
             <li>Number of pages: ${book.pages}</li>
-            <li>Read: ${book.read? 'Yes': 'No'}</li>
+            <li class="read">Read: ${book.read? 'Yes': 'No'}</li>
+            <li>ID: ${book.id}</li>
         </ul>
-        <p>Description: ${book.info()}</p>
+        <button class="read-status">Read? Yes/No</button>
         `;
+
+        div.querySelector('.delete').addEventListener('click', () => {
+            const bookId = div.getAttribute('data-id');
+            removeBook(bookId);
+        });
+
+        const readDisplay = div.querySelector('.read');
+        div.querySelector('.read-status').addEventListener("click", () => {
+            book.toggleRead()
+            readDisplay.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+        });
+
         display.appendChild(div);
     });
 }
 
-displayBook();
+document.querySelector(".form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    addBookToLibrary(title, author, pages, read);
+
+    displayBooks();
+
+    e.target.reset();
+});
